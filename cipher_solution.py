55ed13350158464f50d7ab206eb2c5b69fa0ae5d
Anastasia Babkina <nastya02022008@gmail.com>
2025-09-05 14:02:29 +0500

    cipher solution 2

--- cipher_solution.py
+++ cipher_solution.py
@@ -1,45 +1,42 @@
-# 141788964 - ID посылки
+# 141864540 - ID посылки
 import sys
+from string import digits, ascii_lowercase, ascii_uppercase
 
 
-def helper(string_in: str):
-    """This function finds the correct bracket sequence."""
-    stack: list = []
-    for i in string_in:
-        if stack.count(']') > 0 and stack.count(']') == stack.count('['):
-            break
-        if i != ']':
-            stack.append(i)
-        else:
-            if stack.count(']') != stack.count('['):
-                stack.append(']')
-    return ''.join(stack)
-
-
-def perform_decryption(line: str, recursion_depth_flag: bool = False,
-                       temp: list = None):
+def perform_decryption(line: str):
     """This function decrypts the received instruction."""
-    if temp is None:
-        temp = []
+    temp: str = ''
     value: int = 0
-    digit: list = []
+    digit: str = ''
+    recursion_depth_flag: bool = False
     while value < len(line):
-        if line[value].isdigit():
-            digit.append(line[value])
+        if line[value] in ascii_lowercase or line[value] in ascii_uppercase:
+            temp += line[value]
+        elif line[value] in digits:
+            digit += line[value]
         elif line[value] == '[':
-            recursion_depth_flag = True
-            other: str = helper(line[value:])
-            temp.append((int(''.join(digit)) - 1)
-                        * perform_decryption(other[1:], recursion_depth_flag))
-            digit = []
+            recursion_depth_flag: bool = True
+            index_line_end: int = 0
+            count_bracket: int = 0
+            for index, val in enumerate(line[value + 1:]):
+                if val == '[':
+                    count_bracket += 1
+                elif val == ']' and count_bracket == 0:
+                    index_line_end = index
+                    break
+                elif val == ']':
+                    count_bracket -= 1
+            recursion_result = perform_decryption(line[value + 1:
+                                                       value + 1 +
+                                                       index_line_end])
+            temp += (int(digit) - 1) * recursion_result
+            digit = ''
             recursion_depth_flag = False
-        elif line[value] == ']':
-            if recursion_depth_flag:
-                return ''.join(temp)
         else:
-            temp.append(line[value])
+            if recursion_depth_flag:
+                return temp
         value += 1
-    return ''.join(temp)
+    return temp
 
 
 if __name__ == '__main__':
