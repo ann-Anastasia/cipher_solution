3e077418eef14418ae8d422a5af9de766805959c
Anastasia Babkina <nastya02022008@gmail.com>
2025-09-06 15:15:38 +0500

    cipher solution 3

--- cipher_solution.py
+++ cipher_solution.py
@@ -1,44 +1,27 @@
-# 141864540 - ID посылки
+# 141908380 - ID посылки
 import sys
-from string import digits, ascii_lowercase, ascii_uppercase
+from string import digits, ascii_letters
 
 
-def perform_decryption(line: str):
+def perform_decryption(line: list):
     """This function decrypts the received instruction."""
     temp: str = ''
-    value: int = 0
     digit: str = ''
-    recursion_depth_flag: bool = False
-    while value < len(line):
-        if line[value] in ascii_lowercase or line[value] in ascii_uppercase:
-            temp += line[value]
-        elif line[value] in digits:
-            digit += line[value]
-        elif line[value] == '[':
-            recursion_depth_flag: bool = True
-            index_line_end: int = 0
-            count_bracket: int = 0
-            for index, val in enumerate(line[value + 1:]):
-                if val == '[':
-                    count_bracket += 1
-                elif val == ']' and count_bracket == 0:
-                    index_line_end = index
-                    break
-                elif val == ']':
-                    count_bracket -= 1
-            recursion_result = perform_decryption(line[value + 1:
-                                                       value + 1 +
-                                                       index_line_end])
-            temp += (int(digit) - 1) * recursion_result
+    while len(line) > 0:
+        value: str = line.pop(0)
+        if value in ascii_letters:
+            temp += value
+        elif value in digits:
+            digit += value
+        elif value == '[':
+            recursion_result = perform_decryption(line)
+            temp += int(digit) * recursion_result
             digit = ''
-            recursion_depth_flag = False
-        else:
-            if recursion_depth_flag:
-                return temp
-        value += 1
+        elif value == ']':
+            return temp
     return temp
 
 
 if __name__ == '__main__':
     line_in = sys.stdin.readline().rstrip()
-    print(perform_decryption(line_in))
+    print(perform_decryption(list(line_in)))
